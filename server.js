8196ba90ef11c37279fa8b83a63169c3d50b5906
rutvij <rutvijdoshi07@gmail.com>
2018-02-13 20:18:28 +0530

    Sever setup at localhost 3000

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const app = express();
+const session = require('express-session');
+const mongoose = require('mongoose');
+
+
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Angular index file..
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+// Set port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+// Create the HTTP Server
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`http://localhost:${port}`));
+
